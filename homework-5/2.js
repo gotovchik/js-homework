efb1f0d5d456947f9ba00956d5cb2011e4dd4aba
Игорь Готовчик <gotovchik.igor@yandex.ru>
2023-09-11 12:36:14 +0300

    hw-5 done

---
+++ homework-5/2.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/*
+Необходимо из объекта post, вывести значения, к которым приписан комментарий, 
+в консоль.
+*/
+
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+console.log(post.author);
+post.comments.forEach((element) => {
+  if (element.userId === 10) {
+    console.log(element.rating.dislikes);
+  }
+  if (element.userId === 5) {
+    console.log(element.userId);
+    console.log(element.text);
+  }
+});
